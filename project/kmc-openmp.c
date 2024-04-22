5f615cb5ab2472afed9beadc62a5c396ddaa283f
Karl Schaefer <reyesc02@moravian.edu>
2024-04-22 19:13:45 -0400

    Add empty files for parallel programs

---
+++ project/kmc-openmp.c
@@ -0,0 +1,5 @@
+/**
+ * Parallel implementation of the K-Means Clustering algorithm with OpenMP
+ * to compile and run this file, run the following command:
+ *    gcc -lm -o kmc-serial kmc-serial.c && ./kmc-serial
+*/
