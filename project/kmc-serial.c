d3beb9bd651e4193e25b00e532101891c4a538b0
Karl Schaefer <reyesc02@moravian.edu>
2024-04-23 20:39:11 -0400

    Fixed cluster assignment bug and made arrays dynamically allocated

--- project/kmc-serial.c
+++ project/kmc-serial.c
@@ -8,58 +8,68 @@
 #include <stdio.h>
 #include <math.h>
 #include <time.h>
+#include <string.h>
 
-// grid size constant
-#define GRID_SIZE 128
 // tolerance constant
 #define TOLERANCE 1e-4
 
+/**
+ * Calculate Euclidean Distance function
+ * This function calculates the Euclidean distance between two points.
+ * This function does not square root the sum of the squares of the differences
+ * because the square root is not necessary for comparing distances.
+ * @param data_point: the pointer to the data_points row
+ * @param k_cluster: the pointer to the k_clusters row
+ * @param n_dimensions: the number of dimensions
+ * @return the Euclidean distance between the two points
+*/
 double calculate_euclidean_distance(double* data_point, double* k_cluster, size_t n_dimensions) {
     double sum = 0;
     for (size_t i = 0; i < n_dimensions; i++) {
-        sum += (data_point[i] - k_cluster[i])*(data_point[i] - k_cluster[i]);
+        sum += (data_point[i] - k_cluster[i]) * (data_point[i] - k_cluster[i]);
     }
     return sum;
 }
+int main(int argn, char* argv[]) {
 
-int main(int argc, char* argv[]) {
-    if (argc != 5) {
-        printf("Usage: %s <grid_size> <n_dimensions> <num_data_points> <num_k_clusters>\n", argv[0]);
-        return 1;
+    // declare variables
+    size_t num_data_points = 10;
+    size_t n_dimensions = 2;
+    size_t num_k_clusters = 4;
+    double grid_size = 32;
+
+    // 1. allocate memory for data_points, cluster_id, and k_clusters
+    double** data_points = (double**)malloc(num_data_points * sizeof(double*));
+    for (size_t i = 0; i < num_data_points; i++) {
+        data_points[i] = (double*)malloc(n_dimensions * sizeof(double));
+    }
+    int* cluster_id = (int*)malloc(num_data_points * sizeof(int));
+
+    double** k_clusters = (double**)malloc(num_k_clusters * sizeof(double*));
+    for (size_t i = 0; i < num_k_clusters; i++) {
+        k_clusters[i] = (double*)malloc(n_dimensions * sizeof(double));
     }
 
-    // parse the user input
-    int grid_size = atoi(argv[1]);
-    int n_dimensions = atoi(argv[2]);
-    int num_data_points = atoi(argv[3]);
-    int num_k_clusters = atoi(argv[4]);
-
-    // declare data_points and cluster_id arrays
-    double data_points[num_data_points][n_dimensions];
-    int cluster_id[num_data_points];
-
     // randomly initialize data_points between -GRID_SIZE and GRID_SIZE
-    srand(0);
+    srand(69);
     for (size_t i = 0; i < num_data_points; i++) {
         for (size_t j = 0; j < n_dimensions; j++) {
             data_points[i][j] = (double)rand() / RAND_MAX * 2 * grid_size - grid_size;
         }
         cluster_id[i] = -1;
     }
-
-    // 2. declare num_k_clusters and k_clusters
-    double k_clusters[num_k_clusters][n_dimensions];
-
+    
     // 3. randomly initialize k_clusters from data_points
     for (size_t i = 0; i < num_k_clusters; i++) {
-        size_t index = rand() % num_data_points;
+        size_t rand_index = rand() % num_data_points;
         for (size_t j = 0; j < n_dimensions; j++) {
-            k_clusters[i][j] = data_points[index][j];
+            k_clusters[i][j] = data_points[rand_index][j];
+            printf("k_clusters[%ld][%ld]: %lf\n", i, j, k_clusters[i][j]);
         }
     }
 
     // declare conditions for convergence
-    size_t max_iterations = 256;
+    size_t max_iterations = 300;
     size_t is_k_clusters_changed;
     size_t is_k_cluster_points_changed;
 
@@ -70,10 +80,8 @@ int main(int argc, char* argv[]) {
         
         // 4. assign each data point to the nearest centroid
         for (size_t i = 0; i < num_data_points; i++) {
-            double min_distance;
-            if (cluster_id[i] == -1) {
-                min_distance = INFINITY;
-            } else {
+            double min_distance = INFINITY;
+            if (cluster_id[i] != -1) {
                 min_distance = calculate_euclidean_distance(data_points[i], k_clusters[cluster_id[i]], n_dimensions);
             }
             for (size_t k = 0; k < num_k_clusters; k++) {
@@ -95,19 +103,17 @@ int main(int argc, char* argv[]) {
 
         // 5. recalculate the centroids of the clusters
         for (int i = 0; i < num_k_clusters; i++) {
-            double sums[n_dimensions];
-            size_t num_points = 0;
-            for (int k = 0; k < num_data_points; k++) {
-                if (cluster_id[k] == i) {
-                    for (int j = 0; j < n_dimensions; j++) {
-                        sums[j] += data_points[k][j];
+            for (int j = 0; j < n_dimensions; j++) {
+                double sums = 0;
+                size_t num_points = 0;
+                for (int k = 0; k < num_data_points; k++) {
+                    if (cluster_id[k] == i) {
+                        sums += data_points[k][j];
+                        num_points++;
                     }
-                    num_points++;
                 }
-            }
-            if (num_points > 0) {
-                for (int j = 0; j < n_dimensions; j++) {
-                    double new_value = sums[j] / num_points;
+                if (num_points > 0) {
+                    double new_value = sums / num_points;
                     if (fabs(new_value - k_clusters[i][j]) > TOLERANCE) {
                         k_clusters[i][j] = new_value;
                         is_k_cluster_points_changed = 1;
@@ -140,6 +146,17 @@ int main(int argc, char* argv[]) {
         fprintf(output_file, ",%d\n", cluster_id[i]);
     }
     fclose(output_file);
+
+    // free memory
+    for (size_t i = 0; i < num_data_points; i++) {
+        free(data_points[i]);
+    }
+    free(data_points);
+    free(cluster_id);
+    for (size_t i = 0; i < num_k_clusters; i++) {
+        free(k_clusters[i]);
+    }
+    free(k_clusters);
 
     return 0;
 }
