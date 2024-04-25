08c75fd6cba8e1047f15b6a119d960f527737eda
Anna Huang <huanga@moravian.edu>
2024-04-25 14:00:31 -0400

    add functions

--- project/kmc-serial.c
+++ project/kmc-serial.c
@@ -5,7 +5,6 @@
  * 
 */
 
-
 #include <math.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -17,10 +16,6 @@
 
 // tolerance constant
 #define TOLERANCE 1e-4
-
-// index of latitude and longitude in the data_points
-#define LATITUDE_INDEX 0
-#define LONGITUDE_INDEX 1
 
 /**
  * Calculate Euclidean Distance function
@@ -42,9 +37,109 @@ double calculate_euclidean_distance(double *data_point, double *k_cluster, size_
     return sum;
 }
 
+/**
+ * make an output fuction for the info
+*/
+void write_output_info(char* filename, unsigned long long num_clusters, unsigned long long num_points, int* cluster_id, unsigned long long num_data_points, unsigned long long num_k_clusters, unsigned long long iterations_reached, unsigned long long reason) {
+    FILE *output_file = fopen(filename, "w");
+    if (output_file == NULL)
+    {
+        printf("Error opening file for writing.\n");
+        return;
+    }
+
+    // Print the number of clusters and points
+    fprintf(output_file, "num_clusters\t%llu\nnum_points\t%llu\n\n", num_k_clusters, num_data_points);
+
+    // Initialize an array to count the number of points in each cluster
+    int* points_in_cluster = calloc(num_k_clusters, sizeof(int));
+    for (size_t i = 0; i < num_data_points; i++) {
+        points_in_cluster[cluster_id[i]]++; 
+    }
+
+    // Print the number of points in each cluster
+    fprintf(output_file, "cluster\tnum_points\n");
+    for (size_t i = 0; i < num_k_clusters; i++) {
+        fprintf(output_file, "%zu\t%d\n", i, points_in_cluster[i]);
+    }
+    fprintf(output_file, "\n");
+
+    // Print the reason for convergence
+    char* reason_string;
+    switch(reason) {
+        case 0:
+            reason_string = "max iterations reached";
+            break;
+        case 1:
+            reason_string = "no change in cluster centroids";
+            break;
+        case 2:
+            reason_string = "no change in cluster points";
+            break;
+        default:
+            reason_string = "unknown";
+            break;
+    }
+    
+    fprintf(output_file, "converged after %llu iterations\nconverged due to %s\n", iterations_reached, reason_string);
+
+    // Close the output file
+    fclose(output_file);
+    free(points_in_cluster);
+}
+
+/**
+ * make an output fuction for the data points
+*/
+void write_output_data_points(char* filename, Matrix* data_points, int* cluster_id, unsigned long long num_data_points, unsigned long long n_dimensions) {
+    FILE *output_file = fopen(filename, "w");
+    for (size_t i = 0; i < num_data_points; i++) {
+        for (size_t j = 0; j < n_dimensions; j++) {
+            fprintf(output_file, "%f,", data_points->data[i * n_dimensions + j]);
+        }
+        fprintf(output_file, "%d\n", cluster_id[i]);
+    }
+    fclose(output_file);
+}
+
+/**
+* Parse command line arguments function
+* This function parses the command line arguments
+*/
+void parse_command_line_arguments(int _argn, char *_argv[], unsigned long long *_num_data_points, unsigned long long *_n_dimensions, unsigned long long *_num_k_clusters, double *_grid_size, Matrix **_data_points, int **_cluster_id, Matrix **_k_clusters) {
+    // parse command line arguments
+    if (_argn == 1) {
+        *_data_points = matrix_random_grid(*_num_data_points, *_n_dimensions, *_grid_size);
+        *_k_clusters = matrix_create_raw(*_num_k_clusters, *_n_dimensions);
+    } else if (_argn == 3) {
+        *_data_points = matrix_from_csv_path(_argv[1]);
+        if (*_data_points == NULL) { perror("error reading input"); exit(1); }
+        *_n_dimensions = (*_data_points)->cols;
+        *_num_data_points = (*_data_points)->rows;
+        *_num_k_clusters = atoi(_argv[2]);
+        *_k_clusters = matrix_create_raw(*_num_k_clusters, *_n_dimensions);
+    } else if (_argn == 5 || _argn == 6) {
+        *_num_data_points = atoi(_argv[1]);
+        *_n_dimensions = atoi(_argv[2]);
+        *_num_k_clusters = atoi(_argv[3]);
+        *_grid_size = atof(_argv[4]);
+        srand((_argn == 6) ? atoi(_argv[5]) : time(NULL));
+        *_data_points = matrix_random_grid(*_num_data_points, *_n_dimensions, *_grid_size);
+        *_k_clusters = matrix_create_raw(*_num_k_clusters, *_n_dimensions);
+    } else {
+        printf("Usage: %s <input_file> <num_k_clusters>\n", _argv[0]);
+        printf("Usage: %s <num_data_points> <n_dimensions> <num_k_clusters> <grid_size> [seed]\n", _argv[0]);
+        exit(1);
+    }
+
+    // initialize cluster_id to -1
+    *_cluster_id = (int*)malloc(*_num_data_points * sizeof(int));
+    memset(*_cluster_id, -1, *_num_data_points * sizeof(int));
+}
+
+
 int main(int argn, char *argv[])
 {
-
     // declare variables
     unsigned long long num_data_points = 10;
     unsigned long long n_dimensions = 2;
@@ -56,33 +151,7 @@ int main(int argn, char *argv[])
     Matrix* k_clusters;
 
     // parse command line arguments
-    if (argn == 1) {
-        data_points = matrix_random_grid(num_data_points, n_dimensions, grid_size);
-        k_clusters = matrix_create_raw(num_k_clusters, n_dimensions);
-    } else if (argn == 3) {
-        data_points = matrix_from_csv_path(argv[1]);
-        if (data_points == NULL) { perror("error reading input"); return 1; }
-        n_dimensions = data_points->cols;
-        num_data_points = data_points->rows;
-        num_k_clusters = atoi(argv[2]);
-        k_clusters = matrix_create_raw(num_k_clusters, n_dimensions);
-    } else if (argn == 5 || argn == 6) {
-        num_data_points = atoi(argv[1]);
-        n_dimensions = atoi(argv[2]);
-        num_k_clusters = atoi(argv[3]);
-        grid_size = atof(argv[4]);
-        srand((argn == 6) ? atoi(argv[5]) : time(NULL));
-        data_points = matrix_random_grid(num_data_points, n_dimensions, grid_size);
-        k_clusters = matrix_create_raw(num_k_clusters, n_dimensions);
-    } else {
-        printf("Usage: %s <input_file> <num_k_clusters>\n", argv[0]);
-        printf("Usage: %s <num_data_points> <n_dimensions> <num_k_clusters> <grid_size> [seed]\n", argv[0]);
-        return 1;
-    }
-
-    // initialize cluster_id to -1
-    cluster_id = (int*)malloc(num_data_points * sizeof(int));
-    memset(cluster_id, -1, num_data_points * sizeof(int));
+    parse_command_line_arguments(argn, argv, &num_data_points, &n_dimensions, &num_k_clusters, &grid_size, &data_points, &cluster_id, &k_clusters);
     
     // 3. randomly initialize k_clusters from data_points
     for (size_t i = 0; i < num_k_clusters; i++) {
@@ -175,70 +244,20 @@ int main(int argn, char *argv[])
         }
     }
 
-    // write output info to output-info-[time].txt    
-    char filename[256];
+    // write output info
+    // write to output-info-[time].txt
+    char filename[64];
     sprintf(filename, "output/output-info-%ld.txt", time(NULL));
-    FILE *output_file = fopen(filename, "w");
-    if (output_file == NULL)
-    {
-        printf("Error opening file for writing.\n");
-        return 1;
-    }
+    write_output_info(filename, num_k_clusters, num_data_points, cluster_id, num_data_points, num_k_clusters, iterations_reached, reason);
 
-    // Print the number of clusters and points
-    fprintf(output_file, "num_clusters\t%llu\nnum_points\t%llu\n\n", num_k_clusters, num_data_points);
-
-    // Initialize an array to count the number of points in each cluster
-    int* points_in_cluster = calloc(num_k_clusters, sizeof(int));
-    for (size_t i = 0; i < num_data_points; i++) {
-        points_in_cluster[cluster_id[i]]++; 
-    }
-
-    // Print the number of points in each cluster
-    fprintf(output_file, "cluster\tnum_points\n");
-    for (size_t i = 0; i < num_k_clusters; i++) {
-        fprintf(output_file, "%zu\t%d\n", i, points_in_cluster[i]);
-    }
-    fprintf(output_file, "\n");
-
-    // Print the reason for convergence
-    char* reason_string;
-    switch(reason) {
-        case 0:
-            reason_string = "max iterations reached";
-            break;
-        case 1:
-            reason_string = "no change in cluster centroids";
-            break;
-        case 2:
-            reason_string = "no change in cluster points";
-            break;
-        default:
-            reason_string = "unknown";
-            break;
-    }
-    
-    fprintf(output_file, "converged after %zu iterations\nconverged due to %s\n", iterations_reached, reason_string);
-
-    // Close the output file
-    fclose(output_file);
-
-    // Print data_points and cluster_id
-    sprintf(filename, "output/output-data-points-%ld.csv", time(NULL));
-    output_file = fopen(filename, "w");
-    for (size_t i = 0; i < num_data_points; i++) {
-        for (size_t j = 0; j < n_dimensions; j++) {
-            fprintf(output_file, "%f,", data_points->data[i * n_dimensions + j]);
-        }
-        fprintf(output_file, "%d\n", cluster_id[i]);
-    }
-    fclose(output_file);
+    // write output data points
+    sprintf(filename, "output/output-points-%ld.txt", time(NULL));
+    write_output_data_points(filename, data_points, cluster_id, num_data_points, n_dimensions);
 
     // Free memory
     matrix_free(data_points);
     free(cluster_id);
     matrix_free(k_clusters);
-    free(points_in_cluster);
 
     return 0;
 }
