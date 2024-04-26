d249be2a564efc5818d8e557df99ba99ec867fb7
Karl Schaefer <reyesc02@moravian.edu>
2024-04-25 20:58:41 -0400

    add serial optimizations

--- project/kmc-serial.c
+++ project/kmc-serial.c
@@ -17,6 +17,17 @@
 // tolerance constant
 #define TOLERANCE 1e-4
 
+// global variables
+unsigned long long num_data_points;
+unsigned long long n_dimensions;
+unsigned long long num_k_clusters;
+double grid_size;
+
+// global pointers
+Matrix* data_points;
+int* cluster_id;
+Matrix* k_clusters;
+
 /**
  * Calculate Euclidean Distance function
  * This function calculates the Euclidean distance between two points.
@@ -26,38 +37,39 @@
  * @param k_cluster: the pointer to the k_clusters row
  * @param n_dimensions: the number of dimensions
  * @return the Euclidean distance between the two points
-*/
+ */
 double calculate_euclidean_distance(double* data_point, double* k_cluster, size_t n_dimensions) {
     double sum = 0;
     for (size_t i = 0; i < n_dimensions; i++) {
-        sum += (data_point[i] - k_cluster[i]) * (data_point[i] - k_cluster[i]);
+        double difference = data_point[i] - k_cluster[i];
+        double square = difference * difference;
+        sum += square;
     }
     return sum;
 }
 
-int main(int argn, char* argv[]) {
-    
-    // declare variables
-    unsigned long long num_data_points = 10;
-    unsigned long long n_dimensions = 2;
-    unsigned long long num_k_clusters = 4;
-    double grid_size = 32;
-
-    Matrix* data_points;
-    int* cluster_id;
-    Matrix* k_clusters;
-
-    // parse command line arguments
+/**
+ * Parse Command Line Arguments function
+ * This function parses the command line arguments and initializes
+ * the global variables.
+ * @param argn: the number of arguments
+ * @param argv: the array of arguments
+ * @return true if the command line arguments are valid, false otherwise
+ */
+bool parse_command_line_arguments(int argn, char* argv[]) {
     if (argn == 1) {
+        num_data_points = 32767;
+        n_dimensions = 2;
+        num_k_clusters = 8;
+        grid_size = 1024;
+        srand(0);
         data_points = matrix_random_grid(num_data_points, n_dimensions, grid_size);
-        k_clusters = matrix_create_raw(num_k_clusters, n_dimensions);
-    } else if (argn == 3) {
+    } else if (argn = 3) {
         data_points = matrix_from_csv_path(argv[1]);
-        if (data_points == NULL) { perror("error reading input"); return 1; }
+        if (data_points == NULL) { perror("error reading input"); return false; }
         n_dimensions = data_points->cols;
         num_data_points = data_points->rows;
         num_k_clusters = atoi(argv[2]);
-        k_clusters = matrix_create_raw(num_k_clusters, n_dimensions);
     } else if (argn == 5 || argn == 6) {
         num_data_points = atoi(argv[1]);
         n_dimensions = atoi(argv[2]);
@@ -65,147 +77,207 @@ int main(int argn, char* argv[]) {
         grid_size = atof(argv[4]);
         srand((argn == 6) ? atoi(argv[5]) : time(NULL));
         data_points = matrix_random_grid(num_data_points, n_dimensions, grid_size);
-        k_clusters = matrix_create_raw(num_k_clusters, n_dimensions);
     } else {
         printf("Usage: %s <input_file> <num_k_clusters>\n", argv[0]);
         printf("Usage: %s <num_data_points> <n_dimensions> <num_k_clusters> <grid_size> [seed]\n", argv[0]);
-        return 1;
+        return false;
     }
-
-    // printf("num_data_points: %llu\n", num_data_points);
-    // printf("n_dimensions: %llu\n", n_dimensions);
-    // printf("num_k_clusters: %llu\n", num_k_clusters);
-
-    // if (argn != 3) { printf("Error: Please provide the input file\n"); return 1; }
-    // Matrix* data_points = matrix_from_csv_path(argv[1]);
-    // if (data_points == NULL) { perror("error reading input"); return 1; }
-    // n_dimensions = data_points->cols;
-    // num_data_points = data_points->rows;
-    // num_k_clusters = atoi(argv[2]);
-    // Matrix* k_clusters = matrix_create_raw(num_k_clusters, n_dimensions);
-    // int* cluster_id = (int*)malloc(num_data_points * sizeof(int));
-
-    // printf("num_data_points: %llu\n", num_data_points);
-    // printf("n_dimensions: %llu\n", n_dimensions);
-    // printf("num_k_clusters: %llu\n", num_k_clusters);
-
-    // randomly initialize data_points between -GRID_SIZE and GRID_SIZE
-    // srand(0);
-    // for (size_t i = 0; i < num_data_points; i++) {
-    //     for (size_t j = 0; j < n_dimensions; j++) {
-    //         data_points[i][j] = (double)rand() / RAND_MAX * 2 * grid_size - grid_size;
-    //     }
-    //     cluster_id[i] = -1;
-    // }
-
+    
     // initialize cluster_id to -1
     cluster_id = (int*)malloc(num_data_points * sizeof(int));
     memset(cluster_id, -1, num_data_points * sizeof(int));
 
-    // for (size_t i = 0; i < num_data_points; i++) {
-    //     cluster_id[i] = -1;
-    // }
-    
-    // 3. randomly initialize k_clusters from data_points
+    // randomly initialize k_clusters from data_points
+    k_clusters = matrix_create_raw(num_k_clusters, n_dimensions);
     for (size_t i = 0; i < num_k_clusters; i++) {
         size_t rand_index = rand() % num_data_points;
+        size_t rand_index_n_dimensions = rand_index * n_dimensions;
+        size_t i_n_dimensions = i * n_dimensions;
         for (size_t j = 0; j < n_dimensions; j++) {
-            k_clusters->data[i * n_dimensions + j] = data_points->data[rand_index * n_dimensions + j];
+            k_clusters->data[i_n_dimensions + j] = data_points->data[rand_index_n_dimensions + j];
         }
     }
 
-    // declare conditions for convergence
-    size_t max_iterations = 300;
-    size_t is_k_clusters_changed;
-    size_t is_k_cluster_points_changed;
+    return true;
+}
 
-    // 6. repeat steps 4 and 5 until convergence
-    for (size_t iteration = 0; iteration < max_iterations; iteration++) {
-        is_k_clusters_changed = 0;
-        is_k_cluster_points_changed = 0;
-        
-        // 4. assign each data point to the nearest centroid
-        for (size_t i = 0; i < num_data_points; i++) {
-            double min_distance = INFINITY;
-            int current_cluster_id = cluster_id[i];
-            int i_n_dimensions = i * n_dimensions;
-            if (current_cluster_id != -1) {
-                min_distance = calculate_euclidean_distance(data_points->data + i_n_dimensions, k_clusters->data + current_cluster_id * n_dimensions, n_dimensions);
-            }
-            for (size_t k = 0; k < num_k_clusters; k++) {
-                double distance = calculate_euclidean_distance(data_points->data + i_n_dimensions, k_clusters->data + k * n_dimensions, n_dimensions);
-                if (distance < min_distance) {
-                    min_distance = distance;
-                    cluster_id[i] = k;
-                    is_k_clusters_changed = 1;
-                }
-            }
-        }
-
-        // check for convergence
-        if (!is_k_clusters_changed) {
-            //printf("is_k_clusters_changed: %d\n", is_k_clusters_changed);
-            //printf("Converged at iteration %d\n", iteration);
-            break;
-        }
-
-        // 5. recalculate the centroids of the clusters
-        for (int i = 0; i < num_k_clusters; i++) {
-            // Loop fission for better performance
-            double* sum = (double*)calloc(n_dimensions, sizeof(double));
-            int count = 0;
-            // for (int j = 0; j < n_dimensions; j++) {
-            //     sum[j] = 0;
-            // }
-            for (int k = 0; k < num_data_points; k++) {
-                if (cluster_id[k] == i) {
-                    for (int j = 0; j < n_dimensions; j++) {
-                        sum[j] += data_points->data[k * n_dimensions + j];
-                    }
-                    count++;
-                }
-            }
-            for (int j = 0; j < n_dimensions; j++) {
-                if (count > 0) {
-                    double new_centroid = sum[j] / count;
-                    if (fabs(k_clusters->data[i * n_dimensions + j] - new_centroid) > TOLERANCE) {
-                        k_clusters->data[i * n_dimensions + j] = new_centroid;
-                        is_k_cluster_points_changed = 1;
-                    }
-                }
-            }
-            free(sum);
-        }
-
-        // check for convergence
-        if (!is_k_cluster_points_changed) {
-            //printf("is_k_cluster_points_changed: %d\n", is_k_cluster_points_changed);
-            //printf("Converged at iteration %d\n", iteration);
-            break;
-        }
-    }
-
-    // print the data_points and their x, y, and cluster_id to output.txt separated by a comma
-    // filename is output-date-time.txt in the output folder
-    size_t start_col = 0;
-    size_t end_col = n_dimensions - 1;
-    char filename[256];
-    sprintf(filename, "output/output-%ld.txt", time(NULL));
-    FILE* output_file = fopen(filename, "w");
-    // print the header
-    // latitude, longitude, cluster_id
-    fprintf(output_file, "latitude,longitude,cluster_id\n");
-    //FILE* output_file = fopen("output.txt", "w");
+/**
+ * Assign Data Points to Nearest Cluster function
+ * This function assigns each data point to the nearest cluster.
+ * @return true if the k_clusters are changed, false otherwise
+ */
+bool assign_data_points_to_nearest_cluster() {
+    bool is_k_clusters_changed = false;
     for (size_t i = 0; i < num_data_points; i++) {
-        for (size_t j = start_col; j <= end_col; j++) {
-            fprintf(output_file, "%lf", data_points->data[i * n_dimensions + j]);
-            if (j < end_col) {
-                fprintf(output_file, ",");
+        double min_distance = INFINITY;
+        int current_cluster_id = cluster_id[i];
+        size_t current_cluster_id_n_dimensions = current_cluster_id * n_dimensions;
+        int i_n_dimensions = i * n_dimensions;
+        if (current_cluster_id != -1) {
+            min_distance = calculate_euclidean_distance(data_points->data + i_n_dimensions, k_clusters->data + current_cluster_id_n_dimensions, n_dimensions);
+        }
+        for (size_t k = 0; k < num_k_clusters; k++) {
+            double distance = calculate_euclidean_distance(data_points->data + i_n_dimensions, k_clusters->data + k * n_dimensions, n_dimensions);
+            if (distance < min_distance) {
+                min_distance = distance;
+                cluster_id[i] = k;
+                is_k_clusters_changed = true;
             }
         }
-        fprintf(output_file, ",%d\n", cluster_id[i]);
     }
-    fclose(output_file);
+    return is_k_clusters_changed;
+}
+
+/**
+ * Recalculate Centroids function
+ * This function recalculates the centroids of the clusters.
+ * @return true if the k_cluster points are changed, false otherwise
+ */
+bool recalculate_centroids() {
+    bool is_k_cluster_points_changed = false;
+    for (int i = 0; i < num_k_clusters; i++) {
+        // loop fission for better performance
+        double* sum = (double*)calloc(n_dimensions, sizeof(double));
+        int count = 0;
+        for (int k = 0; k < num_data_points; k++) {
+            if (cluster_id[k] == i) {
+                size_t k_n_dimensions = k * n_dimensions;
+                for (int j = 0; j < n_dimensions; j++) {
+                    sum[j] += data_points->data[k_n_dimensions + j];
+                }
+                count++;
+            }
+        }
+        size_t i_n_dimensions = i * n_dimensions;
+        for (int j = 0; j < n_dimensions; j++) {
+            if (count > 0) {
+                double new_centroid = sum[j] / count;
+                if (fabs(k_clusters->data[i_n_dimensions + j] - new_centroid) > TOLERANCE) {
+                    k_clusters->data[i_n_dimensions + j] = new_centroid;
+                    is_k_cluster_points_changed = true;
+                }
+            }
+        }
+        free(sum);
+    }
+    return is_k_cluster_points_changed;
+}
+
+/**
+ * Run K Means function
+ * This function runs the K Means algorithm.
+ * @return the iteration reached when the algorithm converged
+ */
+int run_kmeans(size_t _max_iterations, int* reason_converged) {
+    size_t max_iterations = _max_iterations;
+    size_t iteration_reached = 0;
+
+    for (size_t iteration = 0; iteration < max_iterations; iteration++) {
+
+        // assign data points to nearest cluster
+        bool is_k_clusters_changed = assign_data_points_to_nearest_cluster();
+
+        // check for convergence
+        if (is_k_clusters_changed == false) {
+            iteration_reached = iteration;
+            *reason_converged = 1;
+            break;
+        }
+
+        // recalculate centroids
+        bool is_k_cluster_points_changed = recalculate_centroids();
+
+        // check for convergence
+        if (is_k_cluster_points_changed == false) {
+            iteration_reached = iteration;
+            *reason_converged = 2;
+            break;
+        }
+    }
+    return iteration_reached;
+}
+
+/**
+ * Print Results function
+ * This function prints the results of the K Means algorithm.
+ */
+void print_results_to_file(int iteration_converged, int reason_converged, double time_taken) {
+    // save timestamp to variable
+    char timestamp[128];
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    sprintf(timestamp, "%d-%d-%d-%d-%d-%d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+
+    // output data_points and cluster_id to file
+    char output_file[128];
+    sprintf(output_file, "output/output-%s-data.csv", timestamp);
+    FILE* file = fopen(output_file, "w");
+    if (file == NULL) { perror("error writing output"); return; }
+    for (size_t i = 0; i < num_data_points; i++) {
+        for (size_t j = 0; j < n_dimensions; j++) {
+            fprintf(file, "%f,", data_points->data[i * n_dimensions + j]);
+        }
+        fprintf(file, "%d\n", cluster_id[i]);
+    }
+    fclose(file);
+
+    // output program info
+    sprintf(output_file, "output/output-%s-info.txt", timestamp);
+    file = fopen(output_file, "w");
+    if (file == NULL) { perror("error writing output"); return; }
+    fprintf(file, "Data Points: %llu\n", num_data_points);
+    fprintf(file, "Dimensions: %llu\n", n_dimensions);
+    fprintf(file, "K Clusters: %llu\n\n", num_k_clusters);
+    fprintf(file, "Time Taken: %f seconds\n\n", time_taken);
+
+    // print each cluster and its number of data points
+    for (size_t i = 0; i < num_k_clusters; i++) {
+        int count = 0;
+        for (size_t j = 0; j < num_data_points; j++) {
+            if (cluster_id[j] == i) {
+                count++;
+            }
+        }
+        fprintf(file, "Cluster %llu: %d\n", i, count);
+    } fprintf(file, "\n");
+
+    // print iteration converged and reason
+    if (reason_converged == 1) {
+        fprintf(file, "Converged after %d iterations because k_clusters did not change\n", iteration_converged);
+    } else if (reason_converged == 2) {
+        fprintf(file, "Converged after %d iterations because k_cluster points did not change\n", iteration_converged);
+    } else {
+        fprintf(file, "Did not converge after %d iterations\n", iteration_converged);
+    }
+    fclose(file);   
+}
+
+/**
+ * Main function
+ * This is the entry point of the program.
+ * @param argn: the number of arguments
+ * @param argv: the array of arguments
+ * @return 0 if the program runs successfully, 1 otherwise
+ */
+int main(int argn, char* argv[]) {
+    
+    // parse command line arguments
+    if(!parse_command_line_arguments(argn, argv)) { return 1; }
+
+    // start timer
+    clock_t start = clock();
+
+    // run k means algorithm
+    size_t max_iterations = 300;
+    int reason_converged = 0;
+    int iteration_converged = run_kmeans(max_iterations, &reason_converged);
+
+    // end timer
+    clock_t end = clock();
+    double time_taken = (double)(end - start) / CLOCKS_PER_SEC;
+
+    // print the results
+    print_results_to_file(iteration_converged, reason_converged, time_taken);
 
     // free memory
     matrix_free(data_points);
